7cade9961b5d18ab3c38e753762213f5508df383
David Eusebio <davidetlove@gmail.com>
2022-03-11 13:21:50 -0500

    modify title and fb pixel

--- pages/aula/[step].jsx
+++ pages/aula/[step].jsx
@@ -35,7 +35,24 @@ const Aula = ({ setLoader }) => {
   return (
     <>
       <Head>
-        <title>Clase {step} | {data[step].title}</title>
+        <title>Clase {step < 5 ? step : 4} | {data[step].title}</title>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '536118997770453');
+          fbq('track', 'PageView');
+          ${step === 1 ? "fbq('track', 'Lead');" : "fbq('trackCustom', 'video" + step + "');"}
+        `
+          }}
+        />
       </Head>
       <main className={styles.cont}>
         <VisitCounter />
@@ -74,24 +91,6 @@ const Aula = ({ setLoader }) => {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'UA-221794490-6');`
-        }}
-      />
-      <Script
-        id='fbpixel'
-        dangerouslySetInnerHTML={{
-          __html: `
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '536118997770453');
-          fbq('track', 'PageView');
-          ${step === 1 ? "fbq('track', 'Lead');" : "fbq('trackCustom', 'video" + step + "');"}
-        `
         }}
       />
     </>
